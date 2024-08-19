5f335dc74ca27995a3fb7e98323fd242c3cfb1af
shahina2020 <shahina2020@gmail.com>
2024-08-19 12:12:01 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import pickle
+from flask import Flask,render_template,request
+app=Flask(__name__)
+model=pickle.load(open('model.pkl','rb'))
+
+#url
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    #prediction=model.predict([[request.form.get('temperature')]])
+    prediction=model.predict([[28]])
+
+    output=round(prediction[0],2)
+    print(output)
+    return render_template('index.html')
+
+if __name__== '__main__':
+    app.run(debug=True)
